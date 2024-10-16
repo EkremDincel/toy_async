a942e92802e0f41bd04603b57e8b7148bb841ef9
EkremDincel <ekremdincel@gmail.com>
2024-10-16 14:57:48 +0300

    Added examples/sync.py

--- examples/call.py
+++ examples/call.py
@@ -1,15 +1,15 @@
-from toy_async import call_delayed, call_later, switch, Scheduler
+from toy_async import call_later, call_soon, switch, Scheduler
 
 
 async def delayed():
 	for i in range(1, 4):
-		call_delayed(lambda i=i: print(i), i)
+		call_later(lambda i=i: print(i), i)
 	print(0)
 
 
 async def instant():
 	for i in range(1, 4):
-		call_later(lambda i=i: print(i))
+		call_soon(lambda i=i: print(i))
 	print(0)
 
 
---
+++ examples/sync.py
@@ -0,0 +1,9 @@
+from toy_async import *
+
+async def red_function():
+	print("Red")
+	await sleep(1)
+	return "Blue"
+
+result = block_on(red_function())
+print(result)
